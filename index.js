7e1125b48944afa55f0656664e19903723322f18
Denny McFarlane <dmack1boss@gmail.com>
2026-02-14 20:07:48 -0600

    save changes

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
 
+const userListEl = document.querySelector("user-list");
+
 async function main() {
     const users = await fetch("https://jsonplaceholder.typicode.com/users");
     const usersData = await users.json();
-    const userListEl = document.querySelector("user-list");
+    console.log(usersData);
+    userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("");
+}
 
-    userListEl.innerHTML = usersData
-    .map(
-        (user) =>
-             `<div class="user-card">
+main();
+    
+       function showUserPost(id) {
+        window.location.href = `${window.location.origin}/user.html`
+       }
+       function userHTML(user) {
+             return `<div class="user-card">
             <div class="user-card__container">
               <h3>${user.name}</h4>
          <p><b>Email:</b>${user.email}</p>
                 <p><b>Phone:</b>${user.phone}</p>
-                <p><b>Website:</b> <a href="${user.website}" target="_blank">${user.website}
+                <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}
                 </a></p>
             </div>
           </div>`
-    )
-    .join("");
+
 }
 
-main();
