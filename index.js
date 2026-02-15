13a9a4243ac9e44a0deb12ca1a65545d3135c7f4
Denny McFarlane <dmack1boss@gmail.com>
2026-02-15 12:25:41 -0600

    save changes

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ async function main() {
 main();
     
        function showUserPost(id) {
+        localStorage.setItem("id", id);
         window.location.href = `${window.location.origin}/user.html`
        }
        function userHTML(user) {
